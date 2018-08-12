f89da08ecf501fb6e931f2de0ac4fcca18471c7f
Phillip Chang <lasagnaphil@snu.ac.kr>
2018-08-12 18:13:03 +0900

    Ship physics

--- main.lua
+++ main.lua
@@ -1,18 +1,26 @@
 require "strict"
+serpent = require "serpent"
 Object = require "classic"
+Camera = require "Camera"
+
+canvas = nil
 
 fontchars = 'abcdefghijklmnopqrstuvwxyz"\'`-_/1234567890!?[](){}.,;:<>+=%#^*~ '
 font = nil
 
-debug = false
+debug = true
 
 images = {}
 world = nil
 ship = nil
 boxes = {}
 player = nil
+camera = nil
 
-gravity = 9.81 * 10
+Constants = {
+    gravity = 9.81 * 10,
+    waterDensity = 1
+}
 
 function table.find_remove_one(tbl, elem)
     for i, e in ipairs(tbl) do
@@ -31,6 +39,18 @@ function table.find_index(tbl, elem)
     return nil
 end
 
+function table.clear(tbl)
+    for i in pairs(tbl) do
+        tbl[i] = nil
+    end
+end
+
+function table.insert_many(tbl, ...)
+    for _, v in ipairs({...}) do
+        tbl[#tbl + 1] = v
+    end
+end
+
 function drawHighlight(fixture, body)
     local shape = fixture:getShape()
     if shape:typeOf("CircleShape") then
@@ -41,6 +61,58 @@ function drawHighlight(fixture, body)
     else
         love.graphics.line(body:getWorldPoints(shape:getPoints()))
     end
+end
+
+function computeSunkVertices(polygonVertices, yb)
+    local vertices = {}
+    local x1, y1, x2, y2
+    x2 = polygonVertices[#polygonVertices - 1]
+    y2 = polygonVertices[#polygonVertices]
+    for i=1, #polygonVertices do
+        if i % 2 == 1 then
+            x1 = x2
+            x2 = polygonVertices[i]
+        elseif i % 2 == 0 then
+            y1 = y2
+            y2 = polygonVertices[i]
+            if y1 > yb and y2 > yb then
+                table.insert_many(vertices, x2, y2)
+            elseif y1 < yb and y2 > yb then
+                local xb = x1 + (yb - y1) / (y2 - y1) * (x2 - x1)
+                table.insert_many(vertices, xb, yb, x2, y2)
+            elseif y1 > yb and y2 < yb then
+                local xb = x2 + (yb - y2) / (y1 - y2) * (x1 - x2)
+                table.insert_many(vertices, xb, yb)
+            end
+        end
+    end
+    if #vertices < 6 then return nil end
+    return vertices
+end
+
+function computeCentroid(vertices)
+    local cx, cy = 0, 0
+    local area = 0
+    local x1, y1, x2, y2
+    x2 = vertices[#vertices - 1]
+    y2 = vertices[#vertices]
+    for i=1, #vertices do
+        if i % 2 == 1 then
+            x1 = x2
+            x2 = vertices[i]
+        elseif i % 2 == 0 then
+            y1 = y2
+            y2 = vertices[i]
+            local cross = x1 * y2 - x2 * y1
+            cx = cx + (x1 + x2) * cross
+            cy = cy + (y1 + y2) * cross
+            area = area + cross
+        end
+    end
+    area = area / 2
+    cx = cx / (6 * area)
+    cy = cy / (6 * area)
+    return cx, cy, area
 end
 
 require "pickable"
@@ -74,7 +146,8 @@ function love.load()
     font = love.graphics.newImageFont("images/pico8_font.png", fontchars, 1)
     font:setFilter("nearest", "nearest")
     love.graphics.setFont(font)
-    love.graphics.setDefaultFilter("nearest", "nearest", 1)
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    canvas = love.graphics.newCanvas(256, 256)
 
     images.ocean = love.graphics.newImage("images/ocean.png")
     images.background = love.graphics.newImage("images/background.png")
@@ -83,7 +156,7 @@ function love.load()
     images.player = love.graphics.newImage("images/player_small.png")
 
     love.physics.setMeter(32)
-    world = love.physics.newWorld(0, gravity)
+    world = love.physics.newWorld(0, Constants.gravity)
     world:setCallbacks(beginContact, endContact, preSolve, postSolve)
 
     ship = Ship()
@@ -91,39 +164,62 @@ function love.load()
         boxes[i] = Box(i * 32 - 16, 0)
     end
     player = Player()
+
+    camera = Camera(256, 256, 256, 256)
 end
 
 function love.update(dt)
     world:update(dt)
     player:update(dt)
     ship:update(dt)
+
+    camera:update(dt)
+    camera:setFollowLerp(0.2)
+    local x, y = player.body:getPosition()
+    camera:follow(x, 128)
+
     if love.keyboard.isDown("escape") then
         love.event.quit()
     end
 end
 
 function love.draw()
-    love.graphics.push()
-    love.graphics.scale(2, 2)
+    love.graphics.setCanvas(canvas)
+    love.graphics.clear()
+
+    -- draw the game
 
     love.graphics.draw(images.background)
+
+    camera:attach()
+
     for _, box in ipairs(boxes) do
         box:draw()
     end
     player:draw()
     ship:draw()
-    love.graphics.draw(images.ocean)
 
     if debug then
         love.graphics.setColor(0, 1, 0)
         for _, body in pairs(world:getBodies()) do
             for _, fixture in pairs(body:getFixtures()) do
-                drawHighlight(fixture:getShape())
+                drawHighlight(fixture, body)
             end
         end
         love.graphics.setColor(1, 1, 1)
     end
-    love.graphics.pop()
+
+    camera:detach()
+
+    love.graphics.draw(images.ocean)
+
+    love.graphics.setCanvas()
+
+    -- Draw the 512x512 canvas
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.setBlendMode('alpha', 'premultiplied')
+    love.graphics.draw(canvas, 0, 0, 0, 2, 2)
+    love.graphics.setBlendMode('alpha')
 end
 
 function love.keypressed(key, scancode, isrepeat)
--- ship.lua
+++ ship.lua
@@ -4,20 +4,46 @@ function Ship:new()
     self.image = images.ship
     self.width = images.ship:getWidth()
     self.height = images.ship:getHeight()
-    self.baseline = 192
+    self.baseline = 210
+
     self.ground = true
+
     self.body = love.physics.newBody(world, 128, self.baseline, "dynamic")
-    local ship_shape = love.physics.newRectangleShape(0, 10, self.width, self.height - 20)
-    self.fixture = love.physics.newFixture(self.body, ship_shape, 1)
+    local shape = love.physics.newRectangleShape(0, 10, self.width, self.height - 20)
+    self.fixture = love.physics.newFixture(self.body, shape, 1)
+    self.fixture:setDensity(0.9)
     self.fixture:setUserData(self)
 end
 
 function Ship:update(dt)
     local x, y = ship.body:getPosition()
-    self.body:applyForce(0, -gravity * self.body:getMass() * (1 + (y - ship.baseline) / ship.height))
+    local shape = ship.fixture:getShape()
+    local vertices = {ship.body:getWorldPoints(shape:getPoints())}
+    self.sunkVertices = computeSunkVertices(vertices, self.baseline)
+    if self.sunkVertices then
+        local cx, cy, sunkArea = computeCentroid(self.sunkVertices)
+        local force = 3 * Constants.gravity * (sunkArea / 32 / 32) * Constants.waterDensity
+        self.body:applyForce(0, -force, cx, cy)
+        self.cx, self.cy = cx, cy
+    else
+        self.cx, self.cy = nil, nil
+    end
+
 end
 
 function Ship:draw()
     local x,y = self.body:getPosition()
     love.graphics.draw(self.image, x, y, self.body:getAngle(), 1, 1, self.width/2, self.height/2)
+
+    if debug and self.sunkVertices and self.cx and self.cy then
+        if #self.sunkVertices >= 6 then
+            love.graphics.setColor(1, 1, 0)
+            love.graphics.polygon("line", unpack(self.sunkVertices))
+        end
+        love.graphics.setColor(0, 0, 1)
+        love.graphics.line(0, self.baseline, 256, self.baseline)
+        love.graphics.setColor(1, 0, 0)
+        love.graphics.circle("fill", self.cx, self.cy, 3, 8)
+        love.graphics.setColor(1, 1, 1)
+    end
 end
